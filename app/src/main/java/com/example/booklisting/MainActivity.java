bda5c08dab24c8501104bd9a7cf7e671c972099f
Vuxajlo <vukmijatovic@gmail.com>
2021-01-01 20:18:49 +0100

    Added "no connetion" empty view

--- app/src/main/java/com/example/booklisting/MainActivity.java
+++ app/src/main/java/com/example/booklisting/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.booklisting;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -28,8 +31,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoaderManager loaderManager = getSupportLoaderManager();
-        // loaderManager.initLoader(1, null, MainActivity.this);
 
 
         //Get the keyword that user entered
@@ -38,15 +39,30 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             @Override
             public void onClick(View view) {
                 TextView searchView = findViewById(R.id.text_input);
+                TextView emptyView = (TextView) findViewById(R.id.empty_list_item);
+                bookList = findViewById(R.id.book_list);
+                bookList.setEmptyView(emptyView);
+                LoaderManager loaderManager = getSupportLoaderManager();
+
 
                 keyword = searchView.getText().toString().trim();
                 if (keyword.length() == 0) {
                     keyword = " ";
                 }
+                ConnectivityManager cm =
+                        (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                boolean isConnected = activeNetwork != null &&
+                        activeNetwork.isConnectedOrConnecting();
 
-                loaderManager.restartLoader(1, null, MainActivity.this);
-
-
+                if (isConnected) {
+                    loaderManager.restartLoader(1, null, MainActivity.this);
+                } else {
+                    if (!(adapter == null)) {
+                        adapter.clear();
+                    }
+                    emptyView.setText(getResources().getString(R.string.no_internet_connection));
+                }
             }
         });
 
