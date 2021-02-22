c62848df70079e0e87b13ad607f0af24641c5dcd
Vuxajlo <vukmijatovic@gmail.com>
2021-02-23 00:34:58 +0100

    corrected

--- app/src/main/java/com/example/booklisting/MainActivity.java
+++ app/src/main/java/com/example/booklisting/MainActivity.java
@@ -6,7 +6,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
